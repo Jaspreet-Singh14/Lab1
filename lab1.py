e58b926f2355f627e55c8017bc0052b0b5dc1632
Jaspreet-Singh14 <160678530+Jaspreet-Singh14@users.noreply.github.com>
2024-02-29 14:56:47 -0500

    Create lab1.py

---
+++ lab1.py
@@ -0,0 +1,9 @@
+def calculate_age():
+    try:
+        user_input = int(input("Enter the birth year: "))
+        age = 2024 - user_input
+        print("Your age is: {}" .format(age))
+    except ValueError:
+        print("Please enter a valid integer for the birth year.")
+
+calculate_age()
